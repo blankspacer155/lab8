b0c83a44f040c41853e68ca9f844a6fcd92d7290
blankspacer155 <monnoi.92@gmail.com>
2021-01-05 15:30:15 +0700

    8-1 subb

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+	double loan,inper,canpay;
+	cout << "Enter initial loan: "; cin>>loan;
+	cout << "Enter interest rate per year (%): ";cin>>inper;
+	cout << "Enter amount you can pay per year: ";cin>>canpay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +21,29 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	int ind=1;
+	do{
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << setw(13) << left << ind; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << inper*loan/100;
+	loan+=(inper*loan/100);
+	cout << setw(13) << left << loan;
+	if(loan>canpay)
+	{
+	cout << setw(13) << left << canpay;
+	loan-=canpay;
+	cout << setw(13) << left << loan;
+	}
+	else
+	{
+	cout << setw(13) << left << loan;
+	loan=0;
+	cout << setw(13) << left << "0.00";
+	}
 	
+	cout << "\n";	
+	ind++;
+	}while(loan>0);
 	return 0;
 }
